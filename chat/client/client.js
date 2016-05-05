cd19c92b60e5a98837f78491ebb8cbf0cb9a6308
Jesús Lucas <jelukas89@gmail.com>
2016-05-05 16:35:08 +0200

    chat funcionando

--- chat/client/client.js
+++ chat/client/client.js
@@ -12,18 +12,24 @@ Router.route('/', function () {
 * Templates
 */
 if (Meteor.isClient) {
-
     Template.messages.helpers({
         messages: function() {
-            return Messages.find({}, { sort: { time: -1}});
+            return Messages.find({}, { sort: { time: -1}, limit: 10}).fetch().reverse();
+        },
+        username: function() {
+            return Router.current().params.query.username;
         }
     });
 
+    Template.registerHelper( 'equals', ( a1, a2 ) => {
+      return a1 == a2;
+    });
+
     Template.input.events = {
-      'keydown input#message' : function (event) {
+      'keydown textarea#message' : function (event) {
         if (event.which == 13) { // 13 is the enter key event
           if (this.username == null || this.username == '' || this.username == undefined){
-            var name = 'Anonymous';
+            var name = 'Anonimo';
           }else{
             var name = this.username;
           }
@@ -36,9 +42,29 @@ if (Meteor.isClient) {
             });
 
             document.getElementById('message').value = '';
+            document.getElementById('message').innerHTML = '';
             message.value = '';
           }
         }
+      },
+      'click button#enviar' : function (event) {
+          if (this.username == null || this.username == '' || this.username == undefined){
+            var name = 'Anonimo';
+          }else{
+            var name = this.username;
+          }
+          var message = document.getElementById('message');
+          if (message.value != '') {
+            Messages.insert({
+              name: name,
+              message: message.value,
+              time: Date.now(),
+            });
+
+            document.getElementById('message').value = '';
+            document.getElementById('message').innerHTML = '';
+            message.value = '';
+          }
       }
     }
 }
